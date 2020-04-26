f6cd37e44694456ebbd00ad955822943c6679bd3
Eglatcova <e.glatcova@yandex.ru>
2020-04-27 02:29:11 +0300

    lesson14

--- script/script.js
+++ script/script.js
@@ -1,20 +1,70 @@
 'use srtict';
-foo = function (str) {
-  if (typeof str !== 'string') {
-    console.log('Это не строка');
-    return;
-  }
-  strNew = str.trim();
-  if (strNew.length > 30) {
-    strNew2 = strNew.slice(0, 30) + '...';
-    console.log(strNew2);
-  } else {
-    console.log(strNew);
-  }
-};
 
-a = '               ggggggggggggggggggg6666666666666666669999           ';
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max)) + 1;
+}
 
-foo(a);
+function game1() {
+  //let rightNumber = 7;
+  rightNumber = getRandomInt(100);
+  number = prompt("Угадай число от 1 до 100");
+  let i = 10;
+  const game2 = function () {
+    if (number === null) {
+      alert('До встречи!')
+      return;
+    }
+    if (isNaN(number) === true || number === '') {
+      number = prompt("Введите число!");
+      return game2();
+    }
+    i = i - 1;
+    if (number > rightNumber) {
+      if (i === 0) {
+        number2 = confirm('Попытки закончились, хотите сыграть еще?'); 
+        i = 10;
+        if (number2 === false) {
+          alert('До встречи!')
+          return;
+        }
+        number = prompt("Угадай число от 1 до 100");
+        return game2();
+      }
+      
+      alert("Загаданное число меньше, осталось попыток" + ' ' + i);
+      number = prompt("Угадай число от 1 до 100");
+      return game2();
 
-//console.log(foo(ty));
+    } else if (number < rightNumber) {
+      if (i === 0) {
+        number2 = confirm('Попытки закончились, хотите сыграть еще?');
+        i = 10;
+        if (number2 === false) {
+          alert('До встречи!')
+          return;
+        }
+        number = prompt("Угадай число от 1 до 100");
+        return game2();
+      }
+
+      alert("Загаданное число больше, осталось попыток" + ' ' + i);
+      number = prompt("Угадай число от 1 до 100");
+      return game2();
+    } else {
+      alert("Вы угадали!");
+      number3 = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+      if (number3 === false) {
+        alert('До встречи!')
+        return;
+      }
+      number = prompt("Угадай число от 1 до 100");
+      rightNumber = getRandomInt(100);
+      i = 10;
+      return game2();
+    }
+  };
+  return game2;
+}
+
+let resultGame = game1();
+resultGame();
