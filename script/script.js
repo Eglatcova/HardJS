0b6cd61d4006d3836c53bd5306cdf63d357bf282
Eglatcova <e.glatcova@yandex.ru>
2020-04-28 01:59:42 +0300

    lesson7

--- script/script.js
+++ script/script.js
@@ -1,70 +1,45 @@
 'use srtict';
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max)) + 1;
-}
+const week = [
 
-function game1() {
-  //let rightNumber = 7;
-  rightNumber = getRandomInt(100);
-  number = prompt("Угадай число от 1 до 100");
-  let i = 10;
-  const game2 = function () {
-    if (number === null) {
-      alert('До встречи!')
-      return;
-    }
-    if (isNaN(number) === true || number === '') {
-      number = prompt("Введите число!");
-      return game2();
-    }
-    i = i - 1;
-    if (number > rightNumber) {
-      if (i === 0) {
-        number2 = confirm('Попытки закончились, хотите сыграть еще?'); 
-        i = 10;
-        if (number2 === false) {
-          alert('До встречи!')
-          return;
-        }
-        number = prompt("Угадай число от 1 до 100");
-        return game2();
-      }
-      
-      alert("Загаданное число меньше, осталось попыток" + ' ' + i);
-      number = prompt("Угадай число от 1 до 100");
-      return game2();
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday'
 
-    } else if (number < rightNumber) {
-      if (i === 0) {
-        number2 = confirm('Попытки закончились, хотите сыграть еще?');
-        i = 10;
-        if (number2 === false) {
-          alert('До встречи!')
-          return;
-        }
-        number = prompt("Угадай число от 1 до 100");
-        return game2();
-      }
+];
 
-      alert("Загаданное число больше, осталось попыток" + ' ' + i);
-      number = prompt("Угадай число от 1 до 100");
-      return game2();
-    } else {
-      alert("Вы угадали!");
-      number3 = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
-      if (number3 === false) {
-        alert('До встречи!')
-        return;
-      }
-      number = prompt("Угадай число от 1 до 100");
-      rightNumber = getRandomInt(100);
-      i = 10;
-      return game2();
-    }
-  };
-  return game2;
-}
+const date = new Date();
 
-let resultGame = game1();
-resultGame();
+const getWeekDay = (date) => {
+  date = new Date();
+  let days = week;
+  let day = date.getDay() - 1;
+  return days[day];
+};
+
+week.forEach((element, index) => {
+
+  const div = document.createElement('div');
+
+  let html = week[index];
+
+  if (index === 5 || index === 6) {
+    html = html.italics();
+    div.innerHTML = html;
+    document.body.append(div);
+
+  } else {
+    div.innerHTML = html;
+    document.body.append(div);
+  }
+
+  if (week[index] === getWeekDay(date)) {
+    html = html.bold();
+    div.innerHTML = html;
+    document.body.append(div);
+  }
+});
